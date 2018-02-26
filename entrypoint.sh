78de94475a815f383d061088de24f7b7a8dea67e
ngerstle <ngerstle@telenordigital.com>
2018-02-26 12:32:02 +0100

    improving Dockerfile- moving to alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 if [ "$accountname" != "demo" ] 
 then
-	./collect_data.sh --account $accountname
+  pipenv run python cloudmapper.py gather --account-name $ACCOUNT
 else
 	cp config.json.demo config.json
 fi
 pipenv run python cloudmapper.py prepare --account $accountname
 pipenv run python cloudmapper.py serve --public
+
