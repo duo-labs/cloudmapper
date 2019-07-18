dbea28c8520be6459af07e1e88236de0c4a52b12
Scott Piper <scott@summitroute.com>
2019-07-18 09:16:46 -0600

    Move security group code into it's own function

--- commands/find_unused.py
+++ commands/find_unused.py
@@ -8,6 +8,39 @@ import json
 
 __description__ = "Find unused resources in accounts"
 
+def find_unused_security_groups(region):
+    used_sgs = set()
+
+    defined_sgs = query_aws(
+        region.account, "ec2-describe-security-groups", region
+    )
+
+    network_interfaces = query_aws(
+        region.account, "ec2-describe-network-interfaces", region
+    )
+
+    defined_sg_set = {}
+
+    for sg in pyjq.all(".SecurityGroups[]", defined_sgs):
+        defined_sg_set[sg["GroupId"]] = sg
+
+    for used_sg in pyjq.all(
+        ".NetworkInterfaces[].Groups[].GroupId", network_interfaces
+    ):
+        used_sgs.add(used_sg)
+
+    unused_sg_ids = set(defined_sg_set) - used_sgs
+    unused_sgs = []
+    for sg_id in unused_sg_ids:
+        unused_sgs.append(
+            {
+                "id": sg_id,
+                "name": defined_sg_set[sg_id]["GroupName"],
+                "description": defined_sg_set[sg_id].get("Description", ""),
+            }
+        )
+    return unused_sgs
+
 
 def run(arguments):
     _, accounts, config = parse_arguments(arguments)
@@ -16,40 +49,10 @@ def run(arguments):
     for account in accounts:
         unused_resources_for_account = []
         for region_json in get_regions(Account(None, account)):
-            unused_resources_for_region = {}
-            used_sgs = set()
-
             region = Region(Account(None, account), region_json)
-            defined_sgs = query_aws(
-                Account(None, account), "ec2-describe-security-groups", region
-            )
 
-            network_interfaces = query_aws(
-                Account(None, account), "ec2-describe-network-interfaces", region
-            )
-
-            defined_sg_set = {}
-
-            for sg in pyjq.all(".SecurityGroups[]", defined_sgs):
-                defined_sg_set[sg["GroupId"]] = sg
-
-            for used_sg in pyjq.all(
-                ".NetworkInterfaces[].Groups[].GroupId", network_interfaces
-            ):
-                used_sgs.add(used_sg)
-
-            unused_sg_ids = set(defined_sg_set) - used_sgs
-            unused_sgs = []
-            for sg_id in unused_sg_ids:
-                unused_sgs.append(
-                    {
-                        "id": sg_id,
-                        "name": defined_sg_set[sg_id]["GroupName"],
-                        "description": defined_sg_set[sg_id].get("Description", ""),
-                    }
-                )
-
-            unused_resources_for_region["security_groups"] = unused_sgs
+            unused_resources_for_region = {}
+            unused_resources_for_region["security_groups"] = find_unused_security_groups(region)
 
             unused_resources_for_account.append(
                 {
