3a44a12202c8714b2bc9a124f6a6705c8fc3e115
Scott Piper <scott@summitroute.com>
2019-07-26 12:07:13 -0600

    Print audit output as markdown for Slack; also includes simple script so you can pipe the audit data into Slack

--- commands/audit.py
+++ commands/audit.py
@@ -25,6 +25,19 @@ def audit_command(accounts, config, args):
             finding = json.loads(str(finding))
             finding['finding_type_metadata']= conf
             print(json.dumps(finding))
+        elif args.markdown:
+            print(
+                "*Audit Finding: [{}] - {}*\\nAccount: {} ({}) - {}\\nDescription: {}\\nResource: `{}`\\nDetails:```{}```".format(
+                    conf["severity"].upper(),
+                    conf["title"],
+                    finding.region.account.name,
+                    finding.region.account.local_id,
+                    finding.region.name,
+                    conf["description"],
+                    finding.resource_id,
+                    str(finding.resource_details).replace('\n', '\\n')
+                )
+            )
         else:
             print(
                 "{} - {} ({}) - {}: {}".format(
@@ -42,6 +55,12 @@ def run(arguments):
     parser.add_argument(
         "--json",
         help="Print the json of the issues",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--markdown",
+        help="Print issue as markdown (for Slack)",
         default=False,
         action="store_true",
     )
---
+++ utils/toslack.py
@@ -0,0 +1,24 @@
+import json
+import requests
+import sys
+import os
+
+# Usage: Set the environment variable SLACK_WEBHOOK to https://hooks.slack.com/services/XXXX/YYYYY
+
+webhook_url = os.environ['SLACK_WEBHOOK']
+
+for line in sys.stdin:
+    line.rstrip()
+    line = line.replace('\\n', '\n')
+    #print(line)
+
+    slack_data = {'text': line}
+
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text))
