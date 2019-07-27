f2d88e31c637898f4d9e0d60997e47dd8c1e88d7
Ryo Okubo <syucream1031@gmail.com>
2019-07-27 13:05:16 +0900

    Enable to filter based on whitelist/blacklist

--- commands/collect.py
+++ commands/collect.py
@@ -272,6 +272,12 @@ def collect(arguments):
         parameters = {}
         for region in region_list["Regions"]:
             dynamic_parameter = None
+            # region filter
+            if arguments.include_regions and region["RegionName"] not in arguments.include_regions:
+                continue
+            if arguments.exclude_regions and region["RegionName"] in arguments.exclude_regions:
+                continue
+
             # Only call universal services in default region
             if runner["Service"] in universal_services:
                 if region["RegionName"] != default_region:
@@ -472,6 +478,20 @@ def run(arguments):
         help="Remove any existing data for the account before gathering",
         action="store_true",
     )
+    parser.add_argument(
+        "--include-regions",
+        help="Collect resources belonging to only specified comma-separated regions",
+        required=False,
+        type=str,
+        dest="include_regions",
+    )
+    parser.add_argument(
+        "--exclude-regions",
+        help="Collect resources excepts specified comma-separated regions; if --include-region specified, it's ignored",
+        required=False,
+        type=str,
+        dest="exclude_regions",
+    )
 
     args = parser.parse_args(arguments)
 
@@ -487,5 +507,14 @@ def run(arguments):
                 )
             )
         args.account_name = get_account(args.account_name, config, args.config)["name"]
+
+    if args.include_regions:
+        args.include_regions = args.include_regions.replace(" ", "").split(",")
+    if args.exclude_regions:
+        # include_region is primary
+        if args.include_regions:
+            args.exclude_regions = []
+        else:
+            args.exclude_regions = args.exclude_regions.replace(" ", "").split(",")
 
     collect(args)
